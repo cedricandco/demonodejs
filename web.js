bc1a9fdf2fe5d9d3f660c069d0cfe65855b83a91
OpenShift guest <cedric.gervasoni@outlook.com>
2013-10-23 13:24:26 -0400

    HTML File Reader

--- web.js
+++ web.js
@@ -1,12 +1,26 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('PLOP PLOP CEDRIC IS THE BEST ???');  
-});
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var http = require('http');
+var fs = require('fs');
+var url = require("url");
+var path = require("path");
+
+http.createServer(function (request, response) {    
+	var uri = url.parse(request.url).pathname;  
+    var filename = path.join(process.cwd(), uri); 
+	console.log('request starting...' + filename);
+	fs.readFile(filename, function(error, content) {
+		console.log('request ending...' + filename);
+		if (error) {
+		console.log('request error...');
+			response.writeHead(500);
+			response.end();
+		}
+		else {
+		console.log('request success...');
+			response.writeHead(200, { 'Content-Type': 'text/html' });
+			response.end(content, 'utf-8');
+		}
+	});
+	
+}).listen(port);
+console.log('Server running ' + port);
